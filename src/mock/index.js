c5efcbb43629ba88ef09760399f5b428c7220224
miujg <1066647329@qq.com>
2020-11-04 16:06:59 +0800

    somechange

--- src/mock/index.js
+++ src/mock/index.js
@@ -5,7 +5,7 @@ import Mock from 'mockjs'
 function createMock() {
     const files = require.context('.', true, /\.js$/)
     files.keys().forEach(key => {
-        if (key === './mockList.js' || key === './index.js') return
+        if (key === './index.js') return
         const item = files(key).default || files(key)
         Mock.mock(item.url, item.type, () => item.cb())
     })
