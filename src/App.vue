f46ecfef30514ccdc1bfbb8cc6b92b0be3854963
qyh <qyh@qyh-mac-mini-mjg.local>
2020-11-27 10:28:31 +0800

    rem初步搭建

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,17 @@
 </template>
 
 <script>
-export default {};
+    export default {
+        mounted() {
+            // 动态设置跟组件字体。屏幕宽度的百分之一
+            document.addEventListener('DOMContentLoaded', function() {
+                document.documentElement.style.fontSize = document.documentElement.clientWidth / 100 + 'px'
+            })
+            window.onresize = function() {
+                document.documentElement.style.fontSize = document.documentElement.clientWidth / 100 + 'px'
+            }
+        }
+    };
 </script>
 
 <style lang="scss">
--- src/containers/home/index.vue
+++ src/containers/home/index.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="user-wrapper">
-    <h1 @click="goUser">个人信息：</h1>
-    <p><span>姓名：</span><span>{{user.name}}</span></p>
-    <p><span>年龄：</span><span>{{user.age}}</span></p>
-    <p><span>邮箱：</span><span>{{user.email}}</span></p>
-    <p><span>简介：</span><span>{{user.desc}}</span></p>
   </div>
 </template>
 
@@ -31,7 +26,5 @@
 </script>
 
 <style lang="scss">
-  .user-wrapper {
-    padding: 20px;
-  }
+  
 </style>
