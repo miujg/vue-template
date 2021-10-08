8d96b86c3fb4700b82bbfb561a291feac03bf9ea
miujg <1066647329@qq.com>
2021-10-08 15:09:32 +0800

    格式化

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>111</div>
+  <div>
+    <h1>xxx</h1>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+interface IObj {
+  name: string
+  age: number
+}
 export default defineComponent({
   setup() {
-    console.log('xxx')
-  },
+    const obj: IObj = {
+      name: 'xxx',
+      age: 26
+    }
+    console.log(obj)
+    const test = (a: string) => {
+      console.log(a)
+    }
+    test('111')
+  }
 })
 </script>
