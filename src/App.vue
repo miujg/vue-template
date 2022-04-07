a4bb7d22ed6253759922e33205b96ee2cb20fe56
miujg <1066647329@qq.com>
2022-04-07 09:26:54 +0800

    some chage

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
-    <router-view></router-view>
+  <router-view></router-view>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            // 动态设置跟组件字体。屏幕宽度的百分之一
-            document.addEventListener('DOMContentLoaded', function() {
-                document.documentElement.style.fontSize = document.documentElement.clientWidth / 100 + 'px'
-            })
-            window.onresize = function() {
-                document.documentElement.style.fontSize = document.documentElement.clientWidth / 100 + 'px'
-            }
-        }
+export default {
+  mounted() {
+    // 动态设置跟组件字体。屏幕宽度的百分之一
+    document.addEventListener("DOMContentLoaded", function () {
+      document.documentElement.style.fontSize =
+        document.documentElement.clientWidth / 100 + "px";
+    });
+    window.onresize = function () {
+      document.documentElement.style.fontSize =
+        document.documentElement.clientWidth / 100 + "px";
     };
+  },
+};
 </script>
 
 <style lang="scss">
-    @import '~styles/_reset.scss';
+@import "~styles/_reset.scss";
 </style>
